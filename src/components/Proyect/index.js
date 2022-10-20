dba6a51eabb67cb1fc231da7cdf2366283643257
Maxi-Rios <maxirecibos182@gmail.com>
2022-10-20 13:29:43 -0300

    proyect responsi

--- src/components/Proyect/index.js
+++ src/components/Proyect/index.js
@@ -4,6 +4,7 @@ import AnimatedLleters from "../AnimatedLetters/index"
 import { useEffect, useState } from "react"
 import porfolioData from "../../Data/proyect.json"
 
+
 const Proyect = () => {
     const [letterClass, setLetterClass] = useState("text-animate")
 
@@ -19,26 +20,31 @@ const Proyect = () => {
 
     const renderPortfolio = (porfolio) => {
         return (
-
             <div className='images-container'>
                 {porfolio.map((prot, key) => {
                     return (
-                        <div className='image-box' key={key}>
-                            <img
-                                src={prot.cover}
-                                alt={prot.title}
-                                className='portfolio-image'
-                            />
-                            <div className="content">
-                                <p className="title">
+                        <div className='portfolio-item' key={key}>
+                            <div className='portfolio-item-image'>
+                                <img src={prot.cover} alt={prot.title} />
+                            </div>
+                            <h3>{prot.title}</h3>
+                            <div className='portfolio-item-cta'>
+                                <button className='btn' onClick={() => window.open(prot.url)}>
+                                    Github
+                                </button>
+                            </div>
+                            <div className="portfolio-overview">
+                                <p>{prot.description}<br />{prot.skill}</p>
+                            </div>
+
+                            {/* <p className="title">
                                     {prot.title}
                                 </p>
                                 <h4 className="description">
                                     {prot.description}
                                 </h4>
                                 <h5 className="description">{prot.skill}</h5>
-                                <button className="btn" onClick={() => window.open(prot.url)}>Github</button>
-                            </div>
+                                <button className="btn" onClick={() => window.open(prot.url)}>Github</button> */}
                         </div>
                     )
                 })}
